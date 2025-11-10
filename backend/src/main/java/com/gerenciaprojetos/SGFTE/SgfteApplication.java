64eaff2c721512a0df2a0a4f6e71ee747c6a3198
eduardo-santos05 <eduardo.santos.ct05@gmail.com>
2025-11-10 20:53:04 -0300

    Primeiro commit

---
+++ backend/src/main/java/com/gerenciaprojetos/SGFTE/SgfteApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciaprojetos.SGFTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfteApplication.class, args);
+	}
+
+}
